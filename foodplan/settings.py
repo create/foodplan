639c66d1d848f9f5bfaba852f18dadf9f1c93b87
alpotapov <al.potapov@hotmail.com>
2014-10-11 01:24:49 +0200

    Database settings and requirements.txt

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
     'main',
 )
 
@@ -59,8 +60,13 @@ WSGI_APPLICATION = 'foodplan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'foodplan',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'foodplan_user',
+        'PASSWORD': 'foodplan_password',
+        'HOST': 'localhost',
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
