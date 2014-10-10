a0c3dfa29bde7e7d79e6e9e8d9df6a2662f684fd
alpotapov <al.potapov@hotmail.com>
2014-10-11 01:57:02 +0200

    Homepage template

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -88,3 +87,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
+
+TEMPLATE_DIRS = (
+    BASE_DIR + '/templates/',
+)
