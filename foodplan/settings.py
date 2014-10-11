5337a0813ce58479ad2c2b9e67d62ab11d7a5088
Jeff <jeffpyke@uw.edu>
2014-10-11 21:38:09 +0200

    Change basedir

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -96,12 +96,14 @@ STATIC_URL = '/static/'
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
+
 ALLOWED_HOSTS = ["*"]
 
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 if os.environ.get('PROD'):
     DATABASES['default'] = dj_database_url.config()
