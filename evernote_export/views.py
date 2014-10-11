9c3f3d8c67a341149a478581c931ba201bddb1d1
alpotapov <al.potapov@hotmail.com>
2014-10-12 01:37:44 +0200

    Exporting recipes and grocery list (without duplicates) to Evernote

--- evernote_export/views.py
+++ evernote_export/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 from main.models import Recipe
 from exporter import EvernoteExporter
+from django import http
+import json
+
 
 def test(request):
-    #exporter = EvernoteExporter(sandbox=True)
-    rs = Recipe.objects.all()
+    exporter = EvernoteExporter(sandbox=True)
+    rs = Recipe.objects.all()[:2]
+    exporter.import_meals(rs)
+
+    return http.HttpResponse("OK")
+
     for r in rs:
-        print r
+        print r.name
+        print '===='
+
+        ds = json.loads(r.ingredients_json)
+        for d in ds:
+            print d
+
+        ss = json.loads(r.steps_json)
+        for s in ss:
+            print s
+
+        print '+++'
+
+
