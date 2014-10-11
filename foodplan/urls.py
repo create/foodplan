229abf6b79aa651dcddb146f27be2012e2801a4c
Jeff <jeffpyke@uw.edu>
2014-10-11 22:11:49 +0200

    Remove URL slashes

--- foodplan/urls.py
+++ foodplan/urls.py
@@ -6,12 +6,12 @@ urlpatterns = patterns('',
     # url(r'^$', 'foodplan.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^signin/', 'main.views.signin'),
-    url(r'^signup/', 'main.views.signup'),
-    url(r'^dashboard/', 'main.views.dashboard'),
+    url(r'^admin', include(admin.site.urls)),
+    url(r'^signin', 'main.views.signin'),
+    url(r'^signup', 'main.views.signup'),
+    url(r'^dashboard', 'main.views.dashboard'),
     url(r'^$', 'main.views.home'),
-    url(r'^pantry/', 'main.views.pantry'),
-    url(r'^improve/', 'main.views.improve')
+    url(r'^pantry', 'main.views.pantry'),
+    url(r'^improve', 'main.views.improve')
 
 )
